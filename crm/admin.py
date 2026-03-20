aa6cc9bc8f956c503823963e11a83c1a8e81b19b
Francisco J Bravo <fjbravo75@hotmail.com>
2026-03-20 06:46:36 +0100

    Implement CRM domain models and initial migration

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Company, Interaction
+
+
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Interaction)
